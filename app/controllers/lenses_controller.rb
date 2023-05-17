83a9b5f62431a184460ad9d80c0f689f5f96402f
umb311 <murakamitks1117@gmail.com>
2023-05-18 02:55:05 +0900

    made form

--- app/controllers/lenses_controller.rb
+++ app/controllers/lenses_controller.rb
@@ -8,9 +8,17 @@ class LensesController < ApplicationController
   end
 
   def create
+    @lens = Lens.new(lens_params)
+    
+    if @lens.save
+      redirect_to lenses_path, notice: 'created'
+    else
+      render :new
+    end
   end
 
   def edit
+    @lens = Lens.find(params[:id])
   end
 
   def show
@@ -18,8 +26,46 @@ class LensesController < ApplicationController
   end
 
   def update
+    @lens = Lens.find(params[:id])
+    @lens.update(lens_params)
+    redirect_to lens_path
   end
 
   def destroy
+    @lens = Lens.find(params[:id])
+    @lens.destroy
+    redirect_to lenses_path, notice: 'destroyed'
+  end
+
+  private
+  def lens_params
+    params.require(:lens).permit(
+      :name,
+      :model_number,
+      :mount,
+      :lens_type,
+      :focal_length,
+      :minimum_focusing_distance,
+      :maximum_focusing_distance,
+      :aperture, :maximum_aperture,
+      :minimum_aperture,
+      :number_of_aperture_blades,
+      :angle_of_view,
+      :maximum_angle_of_view,
+      :minimum_angle_of_view,
+      :minimum_shooting_distance,
+      :minimum_shooting_distance_tele,
+      :minimum_shooting_distance_wide,
+      :lens_construction,
+      :maximum_magnification,
+      :filter_diameter,
+      :stabilization,
+      :teleconverter_14x,
+      :teleconverter_20x,
+      :dimensions,
+      :weight,
+      :sensor_size,
+      :description
+    )
   end
 end
